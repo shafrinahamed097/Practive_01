0330cb7209849af0e1f20a2dc274dfa3ab26a998
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-03-17 03:08:17 +0600

    Working with Request Header

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     function DemoAction(Request $request){
-        return $request->input();
+      $name = $request->header('name');
+      $age = $request->header('age');
+      $nationality = $request->header('nationality');
+      $fcolor = $request->header('fColor');
+      $fSport1 = $request->header('fSport1');
+      $fSport2 = $request->header('fSport2');
+      $fMovie = $request->header('fMovie');
+      $leisureTime = $request->header('leisureTime');
+
+
+
+
+      return "Hello dear audience!
+        My name is {$name}. I'm {$nationality}. I'm {$age} years old.
+        My Favourite color is {$fcolor}. My favourite sports are {$fSport1} and {$fSport2} . 
+        My Favourite movie is {$fMovie} . I enjoy {$leisureTime} during my leisure time.
+
+        That's all about my introduction. I hope to be friends with all of you. Thank you so much
+        for your patience!
+      ";
     }
 }
